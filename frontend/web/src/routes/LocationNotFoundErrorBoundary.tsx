82ded1eb6675fbc968704801195e6ef86bc28d9a
tocoteron <tocoteron@gmail.com>
2022-08-21 03:03:12 +0900

    :sparkles: introduce routing system on web frontend"

---
+++ frontend/web/src/routes/LocationNotFoundErrorBoundary.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { isLocationNotFoundError } from 'rocon/react';
+
+interface Props {
+  children: React.ReactElement
+  fallback: React.ReactElement
+}
+
+interface State {
+  isNotFound: boolean
+}
+
+class LocationNotFoundErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { isNotFound: false }
+  }
+
+  static getDerivedStateFromError(error: unknown): State {
+    return { isNotFound: isLocationNotFoundError(error) }
+  }
+
+  render = () => this.state.isNotFound ?
+    this.props.fallback :
+    this.props.children
+}
+
+export default LocationNotFoundErrorBoundary;
---
+++ frontend/web/src/routes/Routes.tsx
@@ -0,0 +1,9 @@
+import Rocon, { useRoutes } from 'rocon/react';
+import TopPage from '../pages/TopPage';
+
+const routes = Rocon.Path()
+  .exact({ action: () => <TopPage /> })
+
+const Routes: React.FC = () => useRoutes(routes)
+  
+export default Routes;
