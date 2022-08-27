da6de4d15282a18ea0161fa1dfe1721f821e06dd
tocoteron <tocoteron@gmail.com>
2022-08-27 23:47:06 +0900

    :lipstick: enable controls of circle avatar component

--- frontend/web/src/stories/atoms/CircleAvatar.stories.tsx
+++ frontend/web/src/stories/atoms/CircleAvatar.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import CircleAvatar from '../../components/atoms/CircleAvatar'
 import { akikun } from '../../mockdata/twitter'
 
@@ -8,14 +8,24 @@ const CircleAvatarMeta: ComponentMeta<typeof CircleAvatar> = {
 }
 export default CircleAvatarMeta
 
-export const Small: React.FC = () => (
-  <CircleAvatar image={akikun.icon} size="small" />
+const Template: ComponentStory<typeof CircleAvatar> = args => (
+  <CircleAvatar {...args} />
 )
 
-export const Medium: React.FC = () => (
-  <CircleAvatar image={akikun.icon} size="medium" />
-)
+export const Small = Template.bind({})
+Small.args = {
+  image: akikun.icon,
+  size: 'small',
+}
 
-export const Large: React.FC = () => (
-  <CircleAvatar image={akikun.icon} size="large" />
-)
+export const Medium = Template.bind({})
+Medium.args = {
+  image: akikun.icon,
+  size: 'medium',
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  image: akikun.icon,
+  size: 'large',
+}
