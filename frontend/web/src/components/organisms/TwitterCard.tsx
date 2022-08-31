ddadd5e49b606c46c8bae61449b1df496b7e8d5a
tocoteron <tocoteron@gmail.com>
2022-09-01 00:14:43 +0900

    :lipstick: wrap twitter card component by card component

--- frontend/web/src/components/organisms/TwitterCard.tsx
+++ frontend/web/src/components/organisms/TwitterCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TwitterUser from '../molecules/TwitterUser'
 import Text from '../atoms/Text'
 import VStack from '../atoms/VStack'
+import Card from '../atoms/Card'
 
 interface TwitterCardProps {
   icon: string
@@ -19,11 +20,17 @@ const TwitterCard: React.FC<TwitterCardProps> = ({
   date,
 }) => {
   return (
-    <VStack gap="m">
-      <TwitterUser icon={icon} displayName={displayName} userName={userName} />
-      <Text text={content} />
-      <Text text={date} />
-    </VStack>
+    <Card>
+      <VStack gap="m">
+        <TwitterUser
+          icon={icon}
+          displayName={displayName}
+          userName={userName}
+        />
+        <Text text={content} />
+        <Text text={date} />
+      </VStack>
+    </Card>
   )
 }
 
