76e33bd7eee05ffa42416ecaa2b062bb6d303181
tocoteron <tocoteron@gmail.com>
2022-09-07 22:02:19 +0900

    :rotating_light: format code

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 module "main" {
-  source         = "../module"
-  project        = local.project
-  region         = local.region
+  source  = "../module"
+  project = local.project
+  region  = local.region
 }
