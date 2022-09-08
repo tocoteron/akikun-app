a04c2f6f83b2c06806d29635cd471fd524e7f828
tocoteron <tocoteron@gmail.com>
2022-09-09 01:20:18 +0900

    :rotating_light: format code

--- frontend/web/src/mockdata/youtube.ts
+++ frontend/web/src/mockdata/youtube.ts
@@ -1,6 +1,6 @@
-import { Channel, Video } from "~/models/youtube";
+import { Channel, Video } from '~/models/youtube'
 
-export const firstChannel =  {
+export const firstChannel = {
   id: 'UCt9qik4Z-_J-rj3bKKQCeHg',
   url: 'https://www.youtube.com/channel/UCt9qik4Z-_J-rj3bKKQCeHg',
   name: 'アキくんちゃんネル',
@@ -21,8 +21,10 @@ export const firstChannelVideos: Video[] = [
     id: 'ofhpBPzScC8',
     url: 'https://www.youtube.com/watch?v=ofhpBPzScC8',
     title: '【にじさんじ公式バーチャルライバー】鈴谷アキ　配信テスト放送',
-    description: '配信のテストをさせてください！\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
-    thumbnail: 'https://i.ytimg.com/vi/ofhpBPzScC8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAJrUjrKUACzhzBjmsBcY5A9A2_Jw',
+    description:
+      '配信のテストをさせてください！\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
+    thumbnail:
+      'https://i.ytimg.com/vi/ofhpBPzScC8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAJrUjrKUACzhzBjmsBcY5A9A2_Jw',
     date: '2018/02/10 14:46',
     channel: firstChannel,
   },
@@ -30,8 +32,10 @@ export const firstChannelVideos: Video[] = [
     id: 'IHgHBlKOu64',
     url: 'https://www.youtube.com/watch?v=IHgHBlKOu64',
     title: '【LIVE】みんなでスリザリオ！【鈴谷アキ】',
-    description: 'こんばんは！鈴谷アキです！\n今日はみなさんにおすすめしていただいた\nスリザリオをプレイしようと思います！\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
-    thumbnail: 'https://i.ytimg.com/vi/IHgHBlKOu64/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBKcMfXrYSSYo2MVCOBvysXjiYuwQ',
+    description:
+      'こんばんは！鈴谷アキです！\n今日はみなさんにおすすめしていただいた\nスリザリオをプレイしようと思います！\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
+    thumbnail:
+      'https://i.ytimg.com/vi/IHgHBlKOu64/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBKcMfXrYSSYo2MVCOBvysXjiYuwQ',
     date: '2018/02/11 21:30',
     channel: firstChannel,
   },
@@ -39,18 +43,22 @@ export const firstChannelVideos: Video[] = [
     id: 'zC4v7rkXKZc',
     url: 'https://www.youtube.com/watch?v=zC4v7rkXKZc',
     title: '【にじさんじ公式】鈴谷アキ Mirrativ初配信アーカイブ【01】',
-    description: '2/8　21:30～\nMirrativにて初配信させていただきました！\nあたふたしてしまってすみません…。\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
-    thumbnail: 'https://i.ytimg.com/vi/zC4v7rkXKZc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBaA4tAvUwd_W2dC4mrMteOVYhlhg',
-    date: '2018/02/10 18:03', 
+    description:
+      '2/8　21:30～\nMirrativにて初配信させていただきました！\nあたふたしてしまってすみません…。\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
+    thumbnail:
+      'https://i.ytimg.com/vi/zC4v7rkXKZc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBaA4tAvUwd_W2dC4mrMteOVYhlhg',
+    date: '2018/02/10 18:03',
     channel: firstChannel,
   },
   {
     id: 'cCCVOozNCD8',
     url: 'https://www.youtube.com/watch?v=cCCVOozNCD8',
     title: '#01 鈴谷アキ 自己紹介',
-    description: 'どうも！\n性別なんて関係ない、鈴谷アキです！\n\nTwitterでもちょっと載せたけど\n改めて自己紹介をしてみたよ。\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
-    thumbnail: 'https://i.ytimg.com/vi/cCCVOozNCD8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDvXn9LwnpHaWx3sE2tnlSRKnJOg',
-    date: '2018/02/11 12:20', 
+    description:
+      'どうも！\n性別なんて関係ない、鈴谷アキです！\n\nTwitterでもちょっと載せたけど\n改めて自己紹介をしてみたよ。\n\n【twitter】\nhttps://twitter.com/aki_suzuya\n\n【Mirrativ】\nhttps://www.mirrativ.com/user/2329734',
+    thumbnail:
+      'https://i.ytimg.com/vi/cCCVOozNCD8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDvXn9LwnpHaWx3sE2tnlSRKnJOg',
+    date: '2018/02/11 12:20',
     channel: firstChannel,
   },
 ]
@@ -60,8 +68,10 @@ export const secondChannelVideos: Video[] = [
     id: 'YGRKqrEprEo',
     url: 'https://www.youtube.com/watch?v=YGRKqrEprEo',
     title: 'チャンネル名を決めよう決めよう',
-    description: 'にじさんじ所属バーチャルライバー鈴谷アキです！\n\n今日はセカンドチャンネル開設日として\nチャンネル名を皆さんで決めようと思います！\n\n今日中には決められないので\n候補を何個かいただき、後日決めさせていただきます！\n\nよろしくお願いします！\n\nカンちゃん\nのらキウイ様（@nolakiwi）\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
-    thumbnail: 'https://i.ytimg.com/vi/YGRKqrEprEo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCQg_MWueG7ptf5t0EwH-A4JlVsgw',
+    description:
+      'にじさんじ所属バーチャルライバー鈴谷アキです！\n\n今日はセカンドチャンネル開設日として\nチャンネル名を皆さんで決めようと思います！\n\n今日中には決められないので\n候補を何個かいただき、後日決めさせていただきます！\n\nよろしくお願いします！\n\nカンちゃん\nのらキウイ様（@nolakiwi）\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
+    thumbnail:
+      'https://i.ytimg.com/vi/YGRKqrEprEo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCQg_MWueG7ptf5t0EwH-A4JlVsgw',
     date: '2018/12/22 23:02',
     channel: secondChannel,
   },
@@ -69,8 +79,10 @@ export const secondChannelVideos: Video[] = [
     id: 'GoaIsNSGUj4',
     url: 'https://www.youtube.com/watch?v=GoaIsNSGUj4',
     title: 'クリスマスだよ！一緒にケーキを食べよう！',
-    description: 'にじさんじ所属\nバーチャルライバーの鈴谷アキです！\n\nメリークリスマス！\n皆さんどんなクリスマスをお過ごしですか？\nよかったらボクとケーキを食べませんか？\n\nケーキ、揚げ鶏、お飲み物、お持ちください！\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
-    thumbnail: 'https://i.ytimg.com/vi/GoaIsNSGUj4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAvuSild_X_Z-dY-tPX1JAt-H_mwg',
+    description:
+      'にじさんじ所属\nバーチャルライバーの鈴谷アキです！\n\nメリークリスマス！\n皆さんどんなクリスマスをお過ごしですか？\nよかったらボクとケーキを食べませんか？\n\nケーキ、揚げ鶏、お飲み物、お持ちください！\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
+    thumbnail:
+      'https://i.ytimg.com/vi/GoaIsNSGUj4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAvuSild_X_Z-dY-tPX1JAt-H_mwg',
     date: '2018/12/25 21:30',
     channel: secondChannel,
   },
@@ -78,8 +90,10 @@ export const secondChannelVideos: Video[] = [
     id: '8Pm6l73-AS0',
     url: 'https://www.youtube.com/watch?v=8Pm6l73-AS0',
     title: 'ほずクラ🎀🐈～ありったけの夢をかき集め～',
-    description: 'にじさんじ所属\nバーチャルライバーの鈴谷アキです！\n\n今日はちーちゃんと一緒にマインクラフトをします！\n終了時間未定になっておりますので\n無理をせずご視聴くださいますようお願いします！\n（最低2時間はします…！）\n\nまったり過ごしましょう！\n\nちーちゃんのチャンネル\nhttps://www.youtube.com/channel/UCLO9...\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
-    thumbnail: 'https://i.ytimg.com/vi/8Pm6l73-AS0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjTeSbDMlHZVe07J6QJGzDCw3oMQ',
+    description:
+      'にじさんじ所属\nバーチャルライバーの鈴谷アキです！\n\n今日はちーちゃんと一緒にマインクラフトをします！\n終了時間未定になっておりますので\n無理をせずご視聴くださいますようお願いします！\n（最低2時間はします…！）\n\nまったり過ごしましょう！\n\nちーちゃんのチャンネル\nhttps://www.youtube.com/channel/UCLO9...\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
+    thumbnail:
+      'https://i.ytimg.com/vi/8Pm6l73-AS0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjTeSbDMlHZVe07J6QJGzDCw3oMQ',
     date: '2018/12/25 21:30',
     channel: secondChannel,
   },
@@ -87,11 +101,13 @@ export const secondChannelVideos: Video[] = [
     id: 'gB-i64MkZ6w',
     url: 'https://www.youtube.com/watch?v=gB-i64MkZ6w',
     title: '50,000人突破記念衣装のお披露目！',
-    description: 'にじさんじ所属\nバーチャルライバーの鈴谷アキです！\n\n今日は待ちに待ちました50,000人突破記念衣装をお披露目します！\n是非見ていってくださいね！！\n\nねづみどしママ、運営さん\n本当にありがとう…。\n\nカンちゃん\nのらキウイ様（@nolakiwi）\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
-    thumbnail: 'https://i.ytimg.com/vi/gB-i64MkZ6w/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAXOAsAZBU7yxzEQU_ce6gC0-Hmzg',
+    description:
+      'にじさんじ所属\nバーチャルライバーの鈴谷アキです！\n\n今日は待ちに待ちました50,000人突破記念衣装をお披露目します！\n是非見ていってくださいね！！\n\nねづみどしママ、運営さん\n本当にありがとう…。\n\nカンちゃん\nのらキウイ様（@nolakiwi）\n\ntwitter\nhttps://twitter.com/aki_suzuya\n\n------------------------------------------------\nにじさんじについて\n\nお問い合わせ\nhttp://nijisanji.ichikara.co.jp/faq/\n一番下のリンクよりフォームへお進みください。\nもしくは公式Twitter（@nijisanji_app）へDMをお願いします。\n\n公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\nお手紙など送りたい\nhttp://nijisanji.ichikara.co.jp/faq/\nこちらの「お問い合わせ」を読んでいただき\n以下までよろしくお願いします。\n\n〒106-0032\n東京都港区六本木7-18-18\n住友不動産六本木通ビル2F incube内 いちから株式会社 宛\n------------------------------------------------',
+    thumbnail:
+      'https://i.ytimg.com/vi/gB-i64MkZ6w/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAXOAsAZBU7yxzEQU_ce6gC0-Hmzg',
     date: '2018/12/25 21:30',
     channel: secondChannel,
-  }
+  },
 ]
 
 export const videos: Video[] = [...firstChannelVideos, ...secondChannelVideos]
