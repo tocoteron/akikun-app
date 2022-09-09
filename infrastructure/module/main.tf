6f3c9d6225d7dd814c2ce8679af674bbb57f32be
tocoteron <tocoteron@gmail.com>
2022-09-10 01:56:12 +0900

    :bricks: create web backend container

--- infrastructure/module/main.tf
+++ infrastructure/module/main.tf
@@ -2,3 +2,9 @@ resource "google_app_engine_application" "web_frontend" {
   project     = var.project
   location_id = var.region
 }
+
+resource "google_cloud_run_service" "web_backend" {
+  name = "web-backend"
+  project = var.project
+  location = var.region
+}
