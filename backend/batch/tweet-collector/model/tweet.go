b624913a5191cc01688f511cd597bf51fe6d26d1
tocoteron <tocoteron@gmail.com>
2022-09-21 11:52:17 +0900

    :bug: fix names of tweet model fields

--- backend/batch/tweet-collector/model/tweet.go
+++ backend/batch/tweet-collector/model/tweet.go
@@ -4,7 +4,7 @@ import "time"
 
 type Tweet struct {
 	ID        string    `json:"id" firestore:"id"`
-	AuthorId  string    `json:"authorId" firestore:"authorId"`
+	AuthorId  string    `json:"authorId" firestore:"author_id"`
 	Text      string    `json:"text" firestore:"text"`
-	CreatedAt time.Time `json:"createdAt" firestore:"createdAt"`
+	CreatedAt time.Time `json:"createdAt" firestore:"created_at"`
 }
