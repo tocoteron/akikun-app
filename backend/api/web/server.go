a30e513f1dba658463676f2841e024c3d397bcf3
tocoteron <tocoteron@gmail.com>
2022-09-25 02:22:14 +0900

    :sparkles: enable CORS on web backend api

--- backend/api/web/server.go
+++ backend/api/web/server.go
@@ -1,38 +1,69 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/tocoteron/akikun-app/backend/api/web/graph/generated"
 	"github.com/tocoteron/akikun-app/backend/api/web/graph/resolver"
 	"github.com/tocoteron/akikun-app/backend/api/web/repository"
 )
 
-const defaultPort = "8080"
+// Get environment var or default value
+func getEnvOrDefault(key string, defaultValue string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return defaultValue
+}
+
+// Get environment var or panic program
+func getEnvOrPanic(key string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	panic(fmt.Sprintf("%s is not set", key))
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	// Get env vars
+	port := getEnvOrDefault("PORT", "8080")
 
-	// Repositories
+	// Create server
+	e := echo.New()
+	e.HideBanner = true
+
+	// Set middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// Set GraphQL routes
+	e.POST("/graphql", graphqlHandler())
+	e.POST("/playground", graphqlPlaygroundHandler("/graphql"))
+
+	// Start server
+	serverPort := fmt.Sprintf(":%s", port)
+	e.Logger.Fatal(e.Start(serverPort))
+}
+
+// GraphQL
+func graphqlHandler() echo.HandlerFunc {
 	tweetRepo := repository.NewFirestoreTweetRepository(nil, "akikun/twitter/tweets")
-
-	// Root resolver
 	resolver := resolver.NewResolver(tweetRepo)
+	h := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{Resolvers: resolver},
+		),
+	)
+	return echo.WrapHandler(h)
+}
 
-	// Server
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver}))
-
-	// Routes
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("Listen :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+func graphqlPlaygroundHandler(graphqlPath string) echo.HandlerFunc {
+	h := playground.Handler("GraphQL Playground", graphqlPath)
+	return echo.WrapHandler(h)
 }
